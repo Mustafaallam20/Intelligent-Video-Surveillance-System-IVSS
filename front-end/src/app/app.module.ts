20c25070a6dac7d8b57a85203705311d1b299f81
yoyosef74 <84831859+yoyosef74@users.noreply.github.com>
2022-12-03 02:39:26 +0200

    initializations

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,22 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ViewComponent } from './view/view.component';
-import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    SignupComponent,
-    NavbarComponent,
-    ViewComponent,
-    HistoryComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
