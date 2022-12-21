32b3c40962b3ecddc82c779efa7fc5ef28c638f9
muhammad eid <mohamed.eid1014@gmail.com>
2022-12-22 01:08:34 +0200

    fix bugs

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/auth/login/login.component';
-import { SignupComponent } from './components/auth/signup/signup.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { ViewComponent } from './components/view/view.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ViewComponent } from './view/view.component';
+import { HistoryComponent } from './history/history.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './components/home/home.component';
-import { HistoryComponent } from './components/history/history.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +22,7 @@ import { HistoryComponent } from './components/history/history.component';
     SignupComponent,
     NavbarComponent,
     ViewComponent,
-    HistoryComponent
+    HistoryComponent,
 
   ],
   imports: [
