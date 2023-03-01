c482d29a5c30f8982518405b815947022b72bb3b
Mustafa Allam <66793691+Mustafaallam20@users.noreply.github.com>
2023-03-01 02:49:56 +0200

    Update LoginDto.java

--- backend/backend/src/main/java/login_signupDTO/LoginDto.java
+++ backend/backend/src/main/java/login_signupDTO/LoginDto.java
@@ -3,20 +3,23 @@ package login_signupDTO;
 
 import lombok.Data;
 
+
+
 @Data
 public class LoginDto {
+	
+	
+	
     private String usernameOrEmail;
     private String password;
+    
+    
 	public String getUsernameOrEmail() {
 		return usernameOrEmail;
 	}
-	public void setUsernameOrEmail(String usernameOrEmail) {
-		this.usernameOrEmail = usernameOrEmail;
-	}
+	
 	public String getPassword() {
 		return password;
 	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
 }
